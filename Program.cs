17e00495f0dd1ca09cdea847c4b4962dded17521
cjakomeit <cjakomeit@gmail.com>
2022-12-04 16:08:20 -0700

    Initializing commit

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+// Main will start HERE
+
+public static class GameRunner
+{
+
+}
+
+public class Player
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; } = false;
+    public ICommands[] Commands { get; } = new ICommands[3];
+
+    public Player()
+    {
+
+    }
+}
+
+public static class PlayArea
+{
+
+}
+
+public static class Room
+{
+
+}
+
+public record Communicator
+{
+
+}
+
+public struct Fountain
+{
+
+}
+
+// Commands
+
+public interface ICommands
+{
+    void Run(Player player);
+}
+
+public class MoveNorth : ICommands
+{
+    public void Run(Player player) => player.Y += 1;
+}
+
+public class MoveSouth : ICommands
+{
+    public void Run(Player player) => player.Y -= 1;
+}
+
+public class MoveEast : ICommands
+{
+    public void Run(Player player) => player.X += 1;
+}
+
+public class MoveWest : ICommands
+{
+    public void Run(Player player) => player.X -= 1;
+}
+
+public class ToggleFountain : ICommands
+{
+
+}
+
+public class RepeatInfo : ICommands
+{
+
+}
